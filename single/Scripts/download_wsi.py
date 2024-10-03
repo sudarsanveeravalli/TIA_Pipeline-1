920900cf84bfefea7569e5ba0e8c8df11f129628
murale1729 <murale1729@gmail.com>
2024-10-03 14:20:50 -0500

    Update download_wsi.py

--- single/Scripts/download_wsi.py
+++ single/Scripts/download_wsi.py
@@ -9,7 +9,7 @@ os.makedirs(save_dir, exist_ok=True)
 url = "https://tiatoolbox.dcs.warwick.ac.uk/models/slide_graph/cell-composition/TCGA-C8-A278-01Z-00-DX1.188B3FE0-7B20-401A-A6B7-8F1798018162.svs"
 
 # Download and save the WSI
-wsi_path = os.path.join(save_dir, 'smaple.svs')
+wsi_path = os.path.join(save_dir, 'sample.svs')
 data.download_data(url, wsi_path)
 
 print(f"WSI saved at: {wsi_path}")
