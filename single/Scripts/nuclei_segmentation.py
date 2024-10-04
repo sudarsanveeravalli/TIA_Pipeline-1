4e4a0742e15cc067675f43898f4207ebe7f6a58e
murale1729 <murale1729@gmail.com>
2024-10-04 03:05:26 -0500

    Update nuclei_segmentation.py

--- single/Scripts/nuclei_segmentation.py
+++ single/Scripts/nuclei_segmentation.py
@@ -50,7 +50,7 @@ output = segmentor.predict(
     imgs=[args.input],
     save_dir=args.output_dir,
     mode=args.mode,
-    resolution={"mpp": mpp},  # Setting MPP explicitly from metadata or default
+    resolution={"mpp": (mpp,mpp)},  # Setting MPP explicitly from metadata or default
     on_gpu=args.gpu,
     crash_on_exception=True
 )
